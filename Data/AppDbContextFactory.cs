dbcc671d35bfae89135e3b70d7c8e8d58ecfa079
Alexander Ruidiaz <joelarga2020@gmail.com>
2026-04-17 12:48:42 -0500

    sin secretos jkajajaj

---
+++ Data/AppDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Cursos_AI_Back.Data;
+
+namespace Cursos_AI_Back.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseNpgsql(
+                "Host=194.146.13.211;Port=5432;Database=cursos-ia-db;Username=postgres;Password=0000"
+            );
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
